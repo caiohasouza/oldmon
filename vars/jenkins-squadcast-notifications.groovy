6bf63b96cc02915f7a590e3b3cb2db2b0aa5cd7b
caiohasouza <caio@ganex.com.br>
2023-04-26 19:13:51 -0300

    asd

--- vars/jenkins-squadcast-notifications.groovy
+++ vars/jenkins-squadcast-notifications.groovy
@@ -1,5 +1,5 @@
 def runMyPython() {
-  final pythonContent = libraryResource('jenkins-squadcast-notifications.py')
+  final pythonContent = libraryResource('com/mkobit/sharedlib/jenkins-squadcast-notifications.py')
   // There are definitely better ways to do this without having to write to the consumer's workspace
   writeFile(file: 'jenkins-squadcast-notifications.py', text: pythonContent)
   sh('chmod +x jenkins-squadcast-notifications.py && ./jenkins-squadcast-notifications.py')
