f771b78dd9123533cb448be60d7b100c9dd8430d
caiohasouza <caio@ganex.com.br>
2023-04-26 16:15:29 -0300

    asd

--- jenkins-squadcast-notifications.py
+++ jenkins-squadcast-notifications.py
@@ -38,8 +38,7 @@ if __name__ == "__main__":
     job_name = os.environ['JOB_NAME']
     build_url = os.environ['BUILD_URL']
     job_url = os.environ['JOB_URL']
-    job_status = os.environ['CURRENT_BUILD_RESULT']
-    job_status_previous = os.environ['PREVIOUS_BUILD_RESULT']
+    job_status = os.environ['BUILD_STATUS']
     #job_status = os.environ['BUILD_RESULT']
     priority = os.environ.get('PRIORITY', "P3")
 
--- jenkins-squadcast-notifications.sh
+++ jenkins-squadcast-notifications.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-CURRENT_BUILD_RESULT=`echo ${currentBuild.currentResult}`
-PREVIOUS_BUILD_RESULT=`echo ${currentBuild.previousBuild}`
-echo "Current: $CURRENT_BUILD_RESULT"
-echo "PRevious: $PREVIOUS_BUILD_RESULT"
 curl https://raw.githubusercontent.com/caiohasouza/test-oldmonk/master/jenkins-squadcast-notifications.py | python3 - jenkins-squadcast-notifications.py
