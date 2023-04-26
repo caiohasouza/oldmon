24c652ea04f7362fe0471f751661f3eb76b25c48
caiohasouza <caio@ganex.com.br>
2023-04-26 16:05:42 -0300

    asd

--- jenkins-squadcast-notifications.sh
+++ jenkins-squadcast-notifications.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CURRENT_BUILD_RESULT=`echo $currentBuild.currentResult`
-PREVIOUS_BUILD_RESULT=`echo $currentBuild.previousBuild`
+CURRENT_BUILD_RESULT=`echo ${currentBuild.currentResult}`
+PREVIOUS_BUILD_RESULT=`echo ${currentBuild.previousBuild}`
 echo "Current: $CURRENT_BUILD_RESULT"
 echo "PRevious: $PREVIOUS_BUILD_RESULT"
 curl https://raw.githubusercontent.com/caiohasouza/test-oldmonk/master/jenkins-squadcast-notifications.py | python3 - jenkins-squadcast-notifications.py
