96d28fa4c8a33f1e5fb097f1df8aff04d758fb96
caiohasouza <caio@ganex.com.br>
2023-04-27 09:17:06 -0300

    test1

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -1,3 +1,5 @@
+#!/usr/bin/env groovy
 def runMyPython() {
+  echo "test"
   sh "curl https://raw.githubusercontent.com/caiohasouza/test-oldmonk/master/jenkins-squadcast-notifications.py | echo python3 -"
 }
