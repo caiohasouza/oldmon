c10771d4478341e98904e1b2c4e6ac661bff48f6
caiohasouza <caio@ganex.com.br>
2023-05-05 16:50:57 -0300

    asd

--- vars/squadcastNotifier.groovy
+++ vars/squadcastNotifier.groovy
@@ -5,7 +5,7 @@ def call() {
   def BUILD_STATUS = currentBuild.currentResult
   sh "ls -la"
   final file = libraryResource('jenkins-squadcast-notifications.py')
-  writeFile(file: 'jenkins-squadcast-notifications.py', text: pythonContent)
+  writeFile(file: 'jenkins-squadcast-notifications.py', text: file)
   //sh('chmod +x my_file.py && ./my_file.py')
 
   sh "find / -name jenkins-squadcast-notifications.py"
