fc87a25728409f957ec873872969110511fceb6c
caiohasouza <caio@ganex.com.br>
2023-05-05 16:32:11 -0300

    asd

--- vars/squadcastNotifier.groovy
+++ vars/squadcastNotifier.groovy
@@ -9,5 +9,5 @@ def call() {
   sh "printenv"
   sh "ls -la"
   sh "find / -name jenkins-squadcast-notifications.py"
-  sh "curl https://raw.githubusercontent.com/caiohasouza/test-oldmonk/test/jenkins-squadcast-notifications.py | python3 - --url ${env.} --build-number  ${env.BUILD_NUMBER} --job-name ${env.JOB_NAME} --build-url ${env.BUILD_URL} --job-url ${env.JOB_URL} --build-status ${env.BUILD_STATUS} --priority P3"
+  sh "curl https://raw.githubusercontent.com/caiohasouza/test-oldmonk/test/jenkins-squadcast-notifications.py | python3 - --url ${env.SQUADCAST_URL} --build-number  ${env.BUILD_NUMBER} --job-name ${env.JOB_NAME} --build-url ${env.BUILD_URL} --job-url ${env.JOB_URL} --build-status ${env.BUILD_STATUS} --priority P3"
 }
