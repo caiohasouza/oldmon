25211d0ae99c4f4142eaa4a886e1ab4fb0926024
caiohasouza <caio@ganex.com.br>
2023-05-05 15:51:54 -0300

    asd

--- vars/squadcastNotifier.groovy
+++ vars/squadcastNotifier.groovy
@@ -1,6 +1,11 @@
 #!/usr/bin/env groovy
 
 def call() {
-  echo "test"
+  echo "antes"
+  def current = currentBuild.currentResult
+  def previous = currentBuild.previousBuild?.currentResult
+  echo "${current}"
+  echo "${previous}"
+  echo "depois"
   sh "curl https://raw.githubusercontent.com/caiohasouza/test-oldmonk/test/jenkins-squadcast-notifications.py | python3 -"
 }
