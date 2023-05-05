a3f958d17da08365a2498313462f83d4be8ef8f7
caiohasouza <caio@ganex.com.br>
2023-05-05 16:02:39 -0300

    asd

--- vars/squadcastNotifier.groovy
+++ vars/squadcastNotifier.groovy
@@ -5,6 +5,7 @@ def call() {
   def BUILD_STATUS = currentBuild.currentResult
   echo "${BUILD_STATUS}"
   sh "echo ${BUILD_STATUS}"
+  sh "export BUILD_STATUS=${BUILD_STATUS}"
   sh "printenv"
   sh "curl https://raw.githubusercontent.com/caiohasouza/test-oldmonk/test/jenkins-squadcast-notifications.py | python3 -"
 }
