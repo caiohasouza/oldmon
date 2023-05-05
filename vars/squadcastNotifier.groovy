0c74bd2a5f244250eaf22c92eea7d9b300319b75
caiohasouza <caio@ganex.com.br>
2023-05-05 17:00:33 -0300

    asd

--- vars/squadcastNotifier.groovy
+++ vars/squadcastNotifier.groovy
@@ -3,11 +3,15 @@
 def call() {
   echo "antes"
   def BUILD_STATUS = currentBuild.currentResult
-  sh "ls -la"
+  if (!"${env.PRIORITY}"?.trim()) {
+    def PRIORITY = "P3"
+  }
+  sh "echo ${PRIORITY}"
   final file = libraryResource('jenkins-squadcast-notifications.py')
   writeFile(file: 'jenkins-squadcast-notifications.py', text: file)
   //sh('chmod +x my_file.py && ./my_file.py')
+  sh "python3 jenkins-squadcast-notifications.py --url ${env.SQUADCAST_URL} --build-number  ${env.BUILD_NUMBER} --job-name ${env.JOB_NAME} --build-url ${env.BUILD_URL} --job-url ${env.JOB_URL} --build-status ${BUILD_STATUS}"
 
-  sh "find / -name jenkins-squadcast-notifications.py"
-  sh "curl https://raw.githubusercontent.com/caiohasouza/test-oldmonk/test/jenkins-squadcast-notifications.py | python3 - --url ${env.SQUADCAST_URL} --build-number  ${env.BUILD_NUMBER} --job-name ${env.JOB_NAME} --build-url ${env.BUILD_URL} --job-url ${env.JOB_URL} --build-status ${BUILD_STATUS}"
+  //sh "find / -name jenkins-squadcast-notifications.py"
+  //sh "curl https://raw.githubusercontent.com/caiohasouza/test-oldmonk/test/jenkins-squadcast-notifications.py | python3 - --url ${env.//SQUADCAST_URL} --build-number  ${env.BUILD_NUMBER} --job-name ${env.JOB_NAME} --build-url ${env.BUILD_URL} --job-url ${env.JOB_URL} //--build-status ${BUILD_STATUS}"
 }
