22f1d4e2acf69b64d602eeca735177bd87134f48
caiohasouza <caio@ganex.com.br>
2023-05-08 10:29:03 -0300

    asd

--- vars/squadcastNotifier.groovy
+++ vars/squadcastNotifier.groovy
@@ -1,8 +1,20 @@
 #!/usr/bin/env groovy
-def call() {
+def start() {
+  env.BUILD_STATUS = "STARTED"
+  def priority = "${env.PRIORITY}"
+  def additionalArgs
+  if(priority != null) {
+    additionalArgs = "--priority ${env.PRIORITY}"
+  } else {
+    additionalArgs = "--priority P5"
+  }
+  final file = libraryResource('jenkins-squadcast-notifications.py')
+  writeFile(file: 'jenkins-squadcast-notifications.py', text: file)
+  sh "python3 jenkins-squadcast-notifications.py"
+}
+
+def result() {
   env.BUILD_STATUS = currentBuild.currentResult
-  env.BUILD_STATUS2 = currentBuild.currentResult
-  echo "$BUILD_STATUS2"
   def priority = "${env.PRIORITY}"
   def additionalArgs
   if(priority != null) {
